b53ee26ee7d1f3c820457936074d128c686eb87b
prasad1304 <60149065+prasad1304@users.noreply.github.com>
2020-01-25 09:11:27 +0530

    Add files via upload

---
+++ djecommerce/settings/development.py
@@ -0,0 +1,62 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1']
+
+INSTALLED_APPS += [
+    'debug_toolbar'
+]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+# DEBUG TOOLBAR SETTINGS
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+
+def show_toolbar(request):
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'aicteprojectspec@gmail.com'
+EMAIL_HOST_PASSWORD = 'jbqhmtikjnyzdwzk'
+EMAIL_USE_SSL=False
+#LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL='/'
+
+STRIPE_PUBLIC_KEY = 'pk_test_vqlTjvXGqC3R1KGhivPX7LPO'
+STRIPE_SECRET_KEY = 'sk_test_qclYEmXiaCTUlWRANkniLbNH'
+
+
+SITE_KEY='6LdgUMEUAAAAAMnEQE13OSMFZ6Fl7pcO1463Mkif '
+
---
+++ djecommerce/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls', namespace='core')),
+       path('accounts/', include('allauth.urls')),
+       path('corporate/',include('corporate.urls',namespace="corporate"))
+   
+   
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
